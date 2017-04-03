47f8f577d57e538577e1b42750c8ab068e6a653f
Mark Craig <mcraig@emanatewireless.com>
2017-04-03 10:20:20 -0700

    Adds first Go dynamodb example (creates table).

---
+++ go/movies_create_table.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func main() {
+	// create a session
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:   aws.String("us-east-1"),
+		Endpoint: aws.String("http://127.0.0.1:8000"),
+		//EndPoint: aws.String("https://dynamodb.us-east-1.amazonaws.com"),
+	}))
+
+	// create a dynamodb instance
+	db := dynamodb.New(sess)
+
+	// create the api params
+	params := &dynamodb.CreateTableInput{
+		TableName: aws.String("Movies"),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{AttributeName: aws.String("year"), KeyType: aws.String("HASH")},
+			{AttributeName: aws.String("title"), KeyType: aws.String("RANGE")},
+		},
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{AttributeName: aws.String("year"), AttributeType: aws.String("N")},
+			{AttributeName: aws.String("title"), AttributeType: aws.String("S")},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(100),
+		},
+	}
+
+	// create the table
+	resp, err := db.CreateTable(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// print the response data
+	fmt.Println(resp)
+}
