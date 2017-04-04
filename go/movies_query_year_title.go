2f003f32f3e781a288747d03d29259fddb922c63
Mark Craig <mcraig@emanatewireless.com>
2017-04-04 13:56:35 -0700

    Adds Go query example that always queries on a range of attribute values

---
+++ go/movies_query_year_title.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/fitquick/dynamodb-examples/go/types"
+)
+
+func main() {
+	// create an aws session
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:   aws.String("us-east-1"),
+		Endpoint: aws.String("http://127.0.0.1:8000"),
+		//EndPoint: aws.String("https://dynamodb.us-east-1.amazonaws.com"),
+	}))
+
+	// create a dynamodb instance
+	db := dynamodb.New(sess)
+
+	// query parameters
+	year := 1992
+	letter1 := "A"
+	letter2 := "L"
+
+	// create the api params
+	params := &dynamodb.QueryInput{
+		TableName:              aws.String("Movies"),
+		KeyConditionExpression: aws.String("#yr = :yyyy AND title BETWEEN :letter1 AND :letter2"),
+		ExpressionAttributeNames: map[string]*string{
+			"#yr": aws.String("year"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":yyyy": {
+				N: aws.String(strconv.Itoa(year)),
+			},
+			":letter1": {
+				S: aws.String(letter1),
+			},
+			":letter2": {
+				S: aws.String(letter2),
+			},
+		},
+	}
+
+	// read the item
+	resp, err := db.Query(params)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err.Error())
+		return
+	}
+
+	// dump the response data
+	fmt.Println(resp)
+
+	// Unmarshal the slice of dynamodb attribute values
+	// into a slice of custom structs
+	var movies []types.Movie
+	err = dynamodbattribute.UnmarshalListOfMaps(resp.Items, &movies)
+
+	// print the response data
+	for _, m := range movies {
+		fmt.Printf("Movie: '%s' (%d)\n", m.Title, m.Year)
+	}
+}
